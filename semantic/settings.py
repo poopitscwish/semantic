3b719d47d2101a0d23a915c8b7ee6f21ae1a9ba5
poopitscwish <shenya200lalala@mail.ru>
2024-03-29 18:13:39 +0800

    ++

--- semantic/settings.py
+++ semantic/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
